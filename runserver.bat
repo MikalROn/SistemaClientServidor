017fd9bfdc451493c870b3ecb0b9357b6f8e34c0
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2024-05-14 23:00:26 -0300

    Arrumando server

--- runserver.bat
+++ runserver.bat
@@ -5,5 +5,6 @@ copy /y "index.jsp" "apache-tomcat-8.5.100\webapps\ROOT"
 cd .\apache-tomcat-8.5.100\
 
 set JRE_HOME=C:\Program Files (x86)\Java\jre-1.8
+set JDK_HOME=C:\Program Files\Java\jdk-19
 
 .\bin\startup.bat
