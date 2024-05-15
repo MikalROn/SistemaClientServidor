928767836f86056f4fe337aa785bfb87a6cc17cc
__coelho__ <109885900+MikalROn@users.noreply.github.com>
2024-05-14 22:09:59 -0300

    commit  inicial

---
+++ runserver.bat
@@ -0,0 +1,9 @@
+echo "++++++++  Iniciando Servidor ++++++++"
+
+copy /y "index.jsp" "apache-tomcat-8.5.100\webapps"
+
+cd .\apache-tomcat-8.5.100\
+
+set JRE_HOME=C:\Program Files (x86)\Java\jre-1.8
+
+.\bin\startup.bat
